3b7c57281ef32e716175b2eb4479a1f32d4fc848
Jacqueline <jhjzhou@gmail.com>
2022-07-07 11:08:35 -0400

    starting experience section:

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,10 +1,20 @@
+
+// Styling
+import { flexCenterCol, headlineStyling } from './styles/styles'
+
+// MUI
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 
 const Experience = () => {
   return(
-    <Box id='experience' sx={{ width: '100%', minHeight: '100vh', backgroundColor: 'white' }}>
+    <Box id='experience' sx={{ width: '100%', backgroundColor: 'background.default', ...flexCenterCol, mt: -1 }}>
+      {/* Headline */}
+      <Typography variant="h4" sx={ headlineStyling }>
+        Experience
+      </Typography>
+      
       <Box>
         <Paper sx={{ p: 10, m: 1 }}>
           <Typography>
