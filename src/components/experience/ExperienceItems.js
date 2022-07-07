08d6fe46d217596dabe5aa6ec2a6805fbf9b7132
Jacqueline <jhjzhou@gmail.com>
2022-07-07 15:27:11 -0400

    updated experience items

--- src/components/experience/ExperienceItems.js
+++ src/components/experience/ExperienceItems.js
@@ -23,7 +23,8 @@ export const experienceItems = [
     company: 'Beauty News NYC',
     dates: 'Jun 2020 - Present',
     points: [
-      '',
+      'Proactive creation and publishing of 25+ high-quality beauty/lifestyle-related editorial pieces using WordPress',
+      'Establishing and maintaining relationships with various PR agencies and media contacts'
     ]
   },
   {
@@ -31,7 +32,7 @@ export const experienceItems = [
     company: 'SUNY at Geneseo',
     dates: 'Jan 2019 - May 2019',
     points: [
-      '',
+      'Designed and executed lesson plans, leading and teaching ~20 undergraduate students alongside lab instructor',
     ]
   },
   {
@@ -39,7 +40,8 @@ export const experienceItems = [
     company: 'SUNY at Geneseo',
     dates: 'Aug 2018 - May 2019',
     points: [
-      '',
+      'Researched, planned, and executed an ecological study on spider-ant dueling behavior, analyzing data with R',
+      "Awarded the 2019 Geneseo Foundation’s Undergraduate Research Grant and 3rd place Presentation Winner at the 2019 Northeastern Natural History Conference, attended by 500+ scientists"
     ]
   },
 ]
