83fafeea47932bf0106a455c6ca5643037b3b6f3
Jacqueline <jhjzhou@gmail.com>
2022-07-07 19:32:50 -0400

    added imgs to interests

--- src/components/About.js
+++ src/components/About.js
@@ -24,16 +24,16 @@ const About = () => {
           👋🏻 Hey There!
         </Typography>
         <Typography variant='body1' sx={ paragraphStyling }>
-          I'm Jackie 👩🏻‍💻, a former digital marketer turned software engineer. 
+          I'm Jackie, a former digital marketer turned software engineer. 👩🏻‍💻
         </Typography>
         <Typography variant='body1' sx={ paragraphStyling }>
-          While I've enjoyed a few years in digital marketing, I've found myself drawn to software engineering and the endless potential to learn and grow 🌱 in the field. 
+          While I've enjoyed a few years in digital marketing, I've found myself drawn to software engineering and the endless potential to learn and grow in the field.
         </Typography>
         <Typography variant='body1' sx={ paragraphStyling }>
-          After months of self-studying in my free hours before and after work, in 2022 I decided to take the leap and enroll full-time in a Software Engineering bootcamp to further accelerate my learning 🧠 (though I find the more I learn, the more I learn how much more there is I want to learn).
+          After self-studying in my free hours before and after work, in 2022 I decided to take the leap and enroll full-time in a Software Engineering bootcamp to further accelerate my learning (though I find the more I learn, the more I learn how much more there is I want to learn).
         </Typography>
         <Typography variant='body1' sx={ paragraphStyling }>
-          I love the creative and rewarding nature of coding and am excited to challenge myself 💪🏻 by creating and working on projects I can be proud of!
+          I love the creative and rewarding nature of coding and am excited to challenge myself by creating and working on projects I can be proud of! 💪🏻
         </Typography>
       
         {/* Resume */}
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,3 +1,7 @@
+// Images
+import art1 from '../assets/art-preview-1.png'
+import art2 from '../assets/art-preview-2.png'
+import art3 from '../assets/art-preview-3.png'
 
 // MUI
 import Box from '@mui/material/Box'
@@ -19,6 +23,14 @@ const Interests = () => {
         <Typography variant='body1' sx={ paragraphStyling }>
           When I'm away from the computer, I like to spend my time watercolor painting and graphite sketching 👩🏻‍🎨, with my favorite thing to draw at the moment being birds.
         </Typography>
+        
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, mb: 3 }}>
+          <Box component='img' src={art1} alt='art' sx={{ width: { xs: '30%', md: '25%'}, mx: 1 }}/>
+          <Box component='img' src={art2} alt='art' sx={{ width: { xs: '30%', md: '25%'}, mx: 1 }}/>
+          <Box component='img' src={art3} alt='art' sx={{ width: { xs: '30%', md: '25%'}, mx: 1 }}/>
+        </Box>
+        
+
         <Typography variant='body1' sx={ paragraphStyling }>
           I also love to spend time in the kitchen baking breads 🍞 and cookies, and trying new recipes (have any favorites you'd like to share?) inspired by the places I've traveled 🌎.
         </Typography>
