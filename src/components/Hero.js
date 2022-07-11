02cd6867e4732870391285d8ebf47544147d8166
Jacqueline <jhjzhou@gmail.com>
2022-07-11 09:41:48 -0400

    hero headlines fit on mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,10 +9,10 @@ import { flexCenterCol } from './styles/styles.js'
 const Hero = () => {
   return(
     <Box id='hero' sx={{ ...flexCenterCol, height: '100vh', bgcolor: 'background.default', mb: 1 }}>
-      <Typography variant="h2" className="h1-typed-out" color='primary.light' sx={{ fontWeight: 600, maxWidth: 'fit-content' }}> 
+      <Typography variant="h2" component='h1' className="typed-out" color='primary.light' sx={{ fontWeight: 600, maxWidth: 'fit-content', fontSize: { xs: '2.8rem' } }}> 
         Jacqueline Zhou
       </Typography>
-      <Typography variant="h4" color='primary.main' sx={{ fontWeight: 300 }}>
+      <Typography variant="h4" component='h2' color='primary.main' sx={{ fontWeight: 300, fontSize: { xs: '2.1rem' } }}>
         Software Engineer
       </Typography>
 
