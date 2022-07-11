8d77631f87fe0b04bcdbb93563624f3af0f2e888
Jacqueline <jhjzhou@gmail.com>
2022-07-11 09:46:52 -0400

    minor hero tweak

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,10 +9,10 @@ import { flexCenterCol } from './styles/styles.js'
 const Hero = () => {
   return(
     <Box id='hero' sx={{ ...flexCenterCol, height: '100vh', bgcolor: 'background.default', mb: 1 }}>
-      <Typography variant="h2" component='h1' className="typed-out" color='primary.light' sx={{ fontWeight: 600, maxWidth: 'fit-content', fontSize: { xs: '2.8rem' } }}> 
+      <Typography variant="h2" component='h1' className="typed-out" color='primary.light' sx={{ fontWeight: 600, maxWidth: 'fit-content', fontSize: { xs: '2.8rem', sm: '3.8rem' } }}> 
         Jacqueline Zhou
       </Typography>
-      <Typography variant="h4" component='h2' color='primary.main' sx={{ fontWeight: 300, fontSize: { xs: '2.1rem' } }}>
+      <Typography variant="h4" component='h2' color='primary.main' sx={{ fontWeight: 300, fontSize: { xs: '2.1rem', sm: '2.6rem'} }}>
         Software Engineer
       </Typography>
 
