b5eff732bd7bb7b97a0e987d988d0b886883341f
Jacqueline <jhjzhou@gmail.com>
2022-07-13 21:26:55 -0400

    fixed experience tab scrolling and sizing

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -18,7 +18,11 @@ const Experience = () => {
   const [ currentTab, setCurrentTab ] = useState(0)
 
   const handleTabChange = (e) => {
-    setCurrentTab(parseInt(e.target.id))
+    if (e.target.dataset.testid) {
+      e.target.dataset.testid.includes('Left') ? setCurrentTab(currentTab - 1) : setCurrentTab(currentTab + 1)
+    } else if (e.target.id) {
+      setCurrentTab(parseInt(e.target.id))
+    }
   }
 
   const TabPanel = (props) => {
@@ -44,53 +48,57 @@ const Experience = () => {
         </Typography>
       </Box>
 
-      <Fade>
-        <Box color='primary' sx={{ ...cardStyling, display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, width: { xs: '90%', md: '80%' } }}>
+      <Box sx={{ maxWidth: '100%' }}>
+        <Fade>
+          <Box color='primary' sx={{ ...cardStyling, display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, width: { xs: '90%', md: '80%' } }}>
 
-          {/* Tab Menu */}
-          <Tabs
-            orientation="vertical"
-            value={currentTab}
-            variant="scrollable"
-            onClick={handleTabChange}
-            textColor='secondary'
-            sx={{ display: { xs: 'none', sm: 'inline' }, borderRight: 1, borderColor: 'divider', width: 'fit-content', color: 'primary.dark' }}
-          >
-            { experienceItems.map((item, index) => <Tab label={item.title} id={index} key={index} />)}
-          </Tabs>
+            {/* Tab Menu Regular */}
+            <Tabs
+              orientation="vertical"
+              value={currentTab}
+              variant="scrollable"
+              onClick={handleTabChange}
+              textColor='secondary'
+              sx={{ display: { xs: 'none', sm: 'inline' }, borderRight: 1, borderColor: 'divider', width: 'fit-content', color: 'primary.dark' }}
+            >
+              { experienceItems.map((item, index) => <Tab label={item.title} id={index} key={index} />)}
+            </Tabs>
 
-          {/* Tab Menu */}
-          <Tabs
-            orientation="horizontal"
-            value={currentTab}
-            variant="scrollable"
-            onClick={handleTabChange}
-            textColor='secondary'
-            sx={{ display: { xs: 'inline', sm: 'none' }, borderRight: 1, borderColor: 'divider', width: '100%', color: 'primary.dark', mb: 3, overflow: 'scroll' }}
-          >
-            { experienceItems.map((item, index) => <Tab label={item.title} id={index} key={index} wrapped />)}
-          </Tabs>
+            {/* Tab Menu XS/Mobile */}
+            <Tabs
+              orientation="horizontal"
+              value={currentTab}
+              variant="scrollable"
+              scrollButtons
+              allowScrollButtonsMobile
+              onClick={handleTabChange}
+              textColor='secondary'
+              sx={{ display: { xs: 'flex', sm: 'none' }, borderRight: 1, borderColor: 'divider', width: '100%', color: 'primary.dark', mb: 3, overflow: 'scroll' }}
+            >
+              { experienceItems.map((item, index) => <Tab label={item.title} id={index} key={index} wrapped />)}
+            </Tabs>
 
-          {/* Tab Content */}
-          <Box sx={{ width: { xs: '100%', sm: '75%'} }}>
-            { experienceItems.map((item, index) => {
-              return (
-                <TabPanel value={currentTab} index={index} key={index}>
-                  <Typography variant='subtitle1' sx={{ color: 'primary.main', display: 'inline', lineHeight: 1}}>{item.title} @ {item.company}</Typography>
-                  <Typography variant='subtitle2' sx={{ mt: .75 }}>{item.dates}</Typography>
-                  {item.points.map((point, index) => (
-                    <Box key={index} sx={{ mt: .5, ml: { xs: 0, md: 2}, display: 'flex' }}>
-                      <Box sx={{ mr: { xs: 1.5, md: 3}, mt: .25 }}>▹</Box>
-                      <Typography variant='body1' sx={{ lineHeight: { xs: 1.35, md: 1.5 } }}>{point}</Typography>
-                    </Box>
-                  ))}
-                </TabPanel>
-              )
-            })}
+            {/* Tab Content */}
+            <Box sx={{ width: { xs: '100%', sm: '75%'} }}>
+              { experienceItems.map((item, index) => {
+                return (
+                  <TabPanel value={currentTab} index={index} key={index}>
+                    <Typography variant='subtitle1' sx={{ color: 'primary.main', display: 'inline', lineHeight: 1}}>{item.title} @ {item.company}</Typography>
+                    <Typography variant='subtitle2' sx={{ mt: .75 }}>{item.dates}</Typography>
+                    {item.points.map((point, index) => (
+                      <Box key={index} sx={{ mt: .5, ml: { xs: 0, md: 2}, display: 'flex' }}>
+                        <Box sx={{ mr: { xs: 1.5, md: 3}, mt: .25 }}>▹</Box>
+                        <Typography variant='body1' sx={{ lineHeight: { xs: 1.35, md: 1.5 } }}>{point}</Typography>
+                      </Box>
+                    ))}
+                  </TabPanel>
+                )
+              })}
+            </Box>
+            
           </Box>
-          
-        </Box>
-      </Fade>
+        </Fade>
+      </Box>
     </Box>
   )
 }
